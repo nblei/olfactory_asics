74241473d1da3dd39e03e3b981601de6442f4ba8
Nathan <nbleier3@illinois.edu>
2023-02-16 13:22:01 -0600

    same svm as baselines

--- src/svm/svm.sv
+++ src/svm/svm.sv
@@ -2,21 +2,27 @@ module svm
 #(
     localparam type T = logic[15:0],
     localparam int DIMS = 21,
+    localparam int INTER = 6,
     localparam int CLASSES = 3,
     localparam type TC = logic[$clog2(CLASSES)-1:0]
 )
 (
     input clk_i, rstn_i,
-    input  T feats [DIMS][CLASSES],
-    input  T biases      [CLASSES],
+    input  T feats [DIMS][INTER],
+    input  T biases      [INTER],
+    input  T feats2 [INTER][CLASSES],
+    input  T biases2       [CLASSES],
     input  T din   [DIMS],
     output TC class_o
 );
 
-T FEATS [DIMS][CLASSES];
+T FEATS [DIMS][INTER];
 T DIN   [DIMS];
-T BIASES      [CLASSES];
+T BIASES      [INTER];
+T FEATS2  [INTER][CLASSES];
+T BIASES2        [CLASSES];
 T matprod     [CLASSES];
+T matprodi    [INTER];
 TC CLASS, _class;
 assign class_o = CLASS;
 
@@ -24,14 +30,24 @@ always_ff @(posedge clk_i) begin
     FEATS <= feats;
     DIN <= din;
     BIASES <= biases;
+    BIASES2 <= biases2;
+    FEATS2 <= feats2;
     CLASS <= _class;
 end
 
-mlp_layer #(.D1(DIMS), .D2(CLASSES)) u_matmul
+mlp_layer #(.D1(DIMS), .D2(INTER)) u_matmul1
 (
     .din(DIN),
     .weights(FEATS),
     .biases(BIASES),
+    .dout(matprodi)
+);
+
+mlp_layer #(.D1(DIMS), .D2(INTER)) u_matmul2
+(
+    .din(matprodi),
+    .weights(FEATS2),
+    .biases(BIASES2),
     .dout(matprod)
 );
 
